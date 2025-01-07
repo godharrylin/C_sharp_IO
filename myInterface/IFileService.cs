3cd54a758e34226f4d382ed26e3b632940532920
godharrylin <105359745+godharrylin@users.noreply.github.com>
2025-01-07 19:04:54 +0800

    Add some files.
    Add folders "myInterface" and "Property" for need it.

---
+++ myInterface/IFileService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingTool.myInterface
+{
+	public interface IFileService
+	{
+		IEnumerable<string> GetTxtFiles(string directory);
+		void MoveFiles(string sourceDir, string targetDir, IEnumerable<string> files);
+		void DeleteFiles(string directory, string searchPattern);
+	}
+}
